1044e920baaf8be58acc7a803e52a7920a978c18
shivang-16 <shivangyadav121@gmail.com>
2025-06-01 12:47:51 +0530

    refactor(auth): replace cookie-based auth with bearer token

--- src/actions/task_action.ts
+++ src/actions/task_action.ts
@@ -8,7 +8,7 @@ export const createTask = async (task: any) => {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Cookie: `token=${token}`,
+                'Authorization': `Bearer ${token}` 
             },
             credentials: "include",
             body: JSON.stringify(task),
@@ -20,33 +20,33 @@ export const createTask = async (task: any) => {
 };
 
 export const getTasks = async () => {
-    const token = await getCookie("token");
-
-    try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/task`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Cookie: `token=${token}`,
-            },
-            credentials: "include"
-        });
-        return await res.json();
-    } catch (error) {
-        throw new Error(`Error deleting task: ${(error as Error).message}`);
-    }
+  const token = await getCookie("token");
+  
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/task`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}` 
+      },
+      credentials: "include"
+    });
+    return await res.json();
+  } catch (error) {
+    throw new Error(`Error getting tasks: ${(error as Error).message}`);
+  }
 };
 
 export const getTasksByProjectId = async (projectId: string) => {
     const token = await getCookie("token");
-    console.log(projectId, "here");
+    console.log(token , "here is the token ");
 
     try {
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/task/project/${projectId}`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                Cookie: `token=${token}`,
+                'Authorization': `Bearer ${token}` 
             },
             credentials: "include"
         });
@@ -64,7 +64,7 @@ export const updateTask = async (id:string, updates: any) => {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
-                Cookie: `token=${token}`,
+                'Authorization': `Bearer ${token}` 
             },
             credentials: 'include',
             body: JSON.stringify(updates),
@@ -83,7 +83,7 @@ export const deleteTask = async (id: string) => {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                Cookie: `token=${token}`,
+                'Authorization': `Bearer ${token}` 
             },
             credentials: "include"
         });
